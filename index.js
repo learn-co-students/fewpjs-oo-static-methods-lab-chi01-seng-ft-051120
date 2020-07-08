730f9ba55d4ebcc899e95ebca82b2d0236b87138
skandow <kando1sr@gmail.com>
2020-07-08 09:56:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const nonCapitalizedWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+
 class Formatter {
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.substr(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '')
+  }
+
+  static titleize(string) {
+    const splitString = string.split(" ")
+    splitString[0] = splitString[0][0].toUpperCase() + splitString[0].substr(1)
+    for (let i=1; i<splitString.length; i++) {
+      if (nonCapitalizedWords.includes(splitString[i]) === false) {
+        splitString[i] = splitString[i][0].toUpperCase() + splitString[i].substr(1)
+      } 
+    }
+    return splitString.join(" ")
+  }
   //add static methods here
 }
